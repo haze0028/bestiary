eb785684086f3781da3b0c0716f133eec51185d9
haze0028 <haze9greg@gmail.com>
2023-04-02 14:14:44 -0400

    fakedata init

---
+++ fakedata.js
@@ -0,0 +1,39 @@
+const data = [
+    {
+        name: "Doppler",
+        id: 1,
+        type: '',
+        vulnerabilities: ['fire'],
+        resistances: ['lightning'],
+        immunities: [],
+        traits: [],
+        proficiencies: [],
+        bait: [],
+        quirks: [],
+        behaviours: [],
+        abilities: []
+    },
+]
+
+export default data;
+// Checks for investigation
+// Observation (in combat - 1 action)
+
+// Investigating a scene, reading a text (once per short rest), speaking with a character with info, observing the creature for more than 1 minute
+
+// Name: Nature/History DC14
+
+// Type: Nature DC14
+
+// Vulnerability/resistance/Immunities: History/Nature DC14
+// Specific traits (darkvision, fly speed, languages, etc.) : Arcana/History DC14
+
+// Proficiencies: Insight/History DC14
+
+// Bait: Survival DC14
+
+// Quirks (only hurt by an enraged creature, etc.): Arcana DC14
+
+// Behaviour (pack tactics, focus on injured creature, etc.): Nature/History DC14
+
+// Abilities (Death burst, acid blood, etc): Insight/Nature DC14
