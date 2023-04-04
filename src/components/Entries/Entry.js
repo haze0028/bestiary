6c08524bb1fa04e2e73faa0cc41d49c32364d6f1
haze0028 <haze9greg@gmail.com>
2023-04-03 20:31:49 -0400

    removed redundant comp

--- src/components/Entries/Entry.js
+++ src/components/Entries/Entry.js
@@ -1,26 +1,21 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-  >
-    •
-  </Box>
-);
-
 export default function BasicCard({ monster }) {
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
-        <Typography variant="h3" component="h2" gutterBottom>
-          Monster: {monster.name}
+        <Typography
+          variant="h3"
+          component="h2"
+          gutterBottom
+          sx={{ fontFamily: "norse" }}
+        >
+          {monster.name}
         </Typography>
         <>
           {monster.type !== "" && (
