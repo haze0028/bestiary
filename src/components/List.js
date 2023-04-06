216041725b142b319e59c6da0e49812a63b5a8fe
haze0028 <haze9greg@gmail.com>
2023-04-05 22:05:22 -0400

    order functionality updated

--- src/components/List.js
+++ src/components/List.js
@@ -7,55 +7,58 @@ import {
   MenuItem,
   InputLabel,
   Select,
-  Divider,
   ListItem,
   List,
   ListItemIcon,
-  Button,
-  Menu,
   Stack,
+  IconButton,
+  Popover,
 } from "@mui/material";
 import { DRAWER_WIDTH } from "../App";
 import { useEffect, useState } from "react";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import clsx from "clsx";
 
 export default function ListDrawer({ open, data, handleClick }) {
   const [anchorEl, setAnchorEl] = useState(null);
-  const menuOpen = Boolean(anchorEl);
-  const handleClickMenu = (event) => {
-    console.log("fired");
+
+  const handlePopoverOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
-  const handleClickMenuItem = (e, val) => {
-    setSort(val);
-    setAlpha(val === "allAsc" || val === "allDes" ? true : false);
-    setNames(data.slice().sort((a, b) => compareNames(a.name, b.name)));
-    handleClose();
-    console.log("ewstes");
-  };
-  const handleClose = () => {
+
+  const handlePopoverClose = () => {
     setAnchorEl(null);
   };
-  const [sort, setSort] = useState("allAsc");
+
+  const popopen = Boolean(anchorEl);
+
+  const [sort, setSort] = useState("name");
+  const [order, setOrder] = useState(true);
   const [names, setNames] = useState(
     data.slice().sort((a, b) => (a.name > b.name ? 1 : -1))
   );
-  const [alpha, setAlpha] = useState(true);
+  // const [alpha, setAlpha] = useState(true);
   const types = data
     .filter((item) => item.type !== "")
     .map((item) => item.type);
 
-  // function handleChange(e) {
-  //   setSort(val);
-  //   setAlpha(val === "allAsc" || val === "allDes" ? true : false);
-  //   setNames(data.slice().sort((a, b) => compareNames(a.name, b.name)));
-  // }
+  function orderClickHandler() {
+    setOrder(!order);
+    setNames(data.slice().sort((a, b) => compareNames(a.name, b.name)));
+  }
+
+  function handleChange(e) {
+    const val = e.target.value;
+    setSort(val);
+    setNames(data.slice().sort((a, b) => compareNames(a.name, b.name)));
+  }
 
   const compareNames = (a, b) => {
-    switch (sort) {
-      case "allAsc":
+    switch (order) {
+      case true:
         return a > b ? -1 : 1;
-      case "allDes":
+      case false:
         return a > b ? 1 : -1;
       default:
         break;
@@ -81,44 +84,77 @@ export default function ListDrawer({ open, data, handleClick }) {
         })}
       >
         <Typography variant="h2" sx={{ fontWeight: "bold" }}>
-          Monsters
+          Creatures
         </Typography>
-        {/*  <FormControl
-          variant="standard"
-          sx={{
-            width: "90%",
-            mb: 2,
-            "& label.Mui-focused": {
-              color: "brown",
-            },
-            "& .MuiInputBase-root.MuiInput-root::after": {
-              borderBottomColor: "brown",
-            },
-          }}
-        >
-          <InputLabel id="sort-by-label">Sort by</InputLabel>
-          <Select
-            labelId="sort-by-label"
-            id="sort-by-select"
-            value={sort}
-            label="Sort by"
-            onChange={handleChange}
-          >
-            <MenuItem value="allAsc">All A-Z</MenuItem>
-            <MenuItem value="allDes">All Z-A</MenuItem>
-            <Divider />
-            <MenuItem value="typeAsc">Type</MenuItem>
-            {/* <MenuItem value="typeDes">Type Z-A</MenuItem> 
-          </Select>
-        </FormControl> */}
         <Stack
           direction="row"
           justifyContent="space-between"
           alignItems="center"
         >
-          <Typography variant="h3" sx={{ fontWeight: "bold" }}>
-            {sort === "typeAsc" ? "Type" : "All"}
-          </Typography>
+          <FormControl
+            variant="standard"
+            sx={{
+              width: "70%",
+              mb: 2,
+              "& label.Mui-focused": {
+                color: "brown",
+              },
+              "& .MuiInputBase-root.MuiInput-root::after": {
+                borderBottomColor: "brown",
+              },
+            }}
+          >
+            <InputLabel id="sort-by-label">Sort by</InputLabel>
+            <Select
+              labelId="sort-by-label"
+              id="sort-by-select"
+              value={sort}
+              label="Sort by"
+              onChange={handleChange}
+            >
+              <MenuItem value="name">Name</MenuItem>
+              <MenuItem value="type">Type</MenuItem>
+            </Select>
+          </FormControl>
+          <Popover
+            id="mouse-over-popover"
+            sx={{
+              pointerEvents: "none",
+            }}
+            open={popopen}
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            onClose={handlePopoverClose}
+            disableRestoreFocus
+          >
+            <Typography sx={{ p: 1 }}>{order ? "A-Z" : "Z-A"}</Typography>
+          </Popover>
+          <IconButton
+            className={clsx("order-btn", {
+              orderBtnDes: !order,
+            })}
+            sx={{ width: 40, height: 40 }}
+            onClick={orderClickHandler}
+            aria-owns={popopen ? "mouse-over-popover" : undefined}
+            aria-haspopup="true"
+            onMouseEnter={handlePopoverOpen}
+            onMouseLeave={handlePopoverClose}
+          >
+            <FilterListIcon />
+          </IconButton>
+        </Stack>
+        {/* <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
           <Button
             id="sortby-button"
             aria-controls={menuOpen ? "basic-menu" : undefined}
@@ -149,10 +185,10 @@ export default function ListDrawer({ open, data, handleClick }) {
             <MenuItem onClick={(e) => handleClickMenuItem(e, "typeAsc")}>
               Type
             </MenuItem>
-            {/* <MenuItem onClick={()=>handleClickMenu('typeDes')}>Type</MenuItem> */}
+            {/* <MenuItem onClick={()=>handleClickMenu('typeDes')}>Type</MenuItem>
           </Menu>
-        </Stack>
-        {alpha ? (
+        </Stack> */}
+        {sort === "name" ? (
           <>
             <List>
               {names.map((item) => {
