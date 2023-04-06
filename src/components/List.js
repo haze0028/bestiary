ded21e292e09b27f4cee9e4ba64b0ffdfcd54e55
haze0028 <haze9greg@gmail.com>
2023-04-06 17:27:23 -0400

    list scrollbar, sorting finished

--- src/components/List.js
+++ src/components/List.js
@@ -16,11 +16,14 @@ import {
 } from "@mui/material";
 import { DRAWER_WIDTH } from "../App";
 import { useEffect, useState } from "react";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import FilterListIcon from "@mui/icons-material/FilterList";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import CloseIcon from "@mui/icons-material/Close";
+import CancelIcon from "@mui/icons-material/Cancel";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
 import clsx from "clsx";
 
-export default function ListDrawer({ open, data, handleClick }) {
+export default function ListDrawer({ open, data, handleClick, handleClose }) {
   const [anchorEl, setAnchorEl] = useState(null);
 
   const handlePopoverOpen = (event) => {
@@ -35,35 +38,17 @@ export default function ListDrawer({ open, data, handleClick }) {
 
   const [sort, setSort] = useState("name");
   const [order, setOrder] = useState(true);
-  const [names, setNames] = useState(
-    data.slice().sort((a, b) => (a.name > b.name ? 1 : -1))
-  );
-  // const [alpha, setAlpha] = useState(true);
-  const types = data
-    .filter((item) => item.type !== "")
-    .map((item) => item.type);
+  const names = data.slice().sort((a, b) => (a.name > b.name ? 1 : -1));
+  const types = [...new Set(data.map((item) => item.type))];
 
   function orderClickHandler() {
     setOrder(!order);
-    setNames(data.slice().sort((a, b) => compareNames(a.name, b.name)));
   }
 
   function handleChange(e) {
     const val = e.target.value;
     setSort(val);
-    setNames(data.slice().sort((a, b) => compareNames(a.name, b.name)));
   }
-
-  const compareNames = (a, b) => {
-    switch (order) {
-      case true:
-        return a > b ? -1 : 1;
-      case false:
-        return a > b ? 1 : -1;
-      default:
-        break;
-    }
-  };
 
   useEffect(() => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -79,13 +64,23 @@ export default function ListDrawer({ open, data, handleClick }) {
           "& .MuiPaper-root": {
             width: DRAWER_WIDTH,
             backgroundColor: "transparent",
-            padding: theme.spacing(2),
+            paddingLeft: theme.spacing(2),
+            paddingRight: theme.spacing(2),
           },
         })}
       >
-        <Typography variant="h2" sx={{ fontWeight: "bold" }}>
-          Creatures
-        </Typography>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Typography variant="h2" sx={{ fontWeight: "bold" }}>
+            Creatures
+          </Typography>
+          <IconButton onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </Stack>
         <Stack
           direction="row"
           justifyContent="space-between"
@@ -150,111 +145,96 @@ export default function ListDrawer({ open, data, handleClick }) {
             <FilterListIcon />
           </IconButton>
         </Stack>
-        {/* <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <Button
-            id="sortby-button"
-            aria-controls={menuOpen ? "basic-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={menuOpen ? "true" : undefined}
-            onClick={handleClickMenu}
-            variant="outlined"
-            color="success"
-            sx={{ mr: 3, maxHeight: "80%" }}
-          >
-            Sort By
-          </Button>
-          <Menu
-            id="sortby-menu"
-            anchorEl={anchorEl}
-            open={menuOpen}
-            // onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-          >
-            <MenuItem onClick={(e) => handleClickMenuItem(e, "allAsc")}>
-              All A-Z
-            </MenuItem>
-            <MenuItem onClick={(e) => handleClickMenuItem(e, "allDes")}>
-              All Z-A
-            </MenuItem>
-            <MenuItem onClick={(e) => handleClickMenuItem(e, "typeAsc")}>
-              Type
-            </MenuItem>
-            {/* <MenuItem onClick={()=>handleClickMenu('typeDes')}>Type</MenuItem>
-          </Menu>
-        </Stack> */}
-        {sort === "name" ? (
-          <>
-            <List>
-              {names.map((item) => {
-                return (
-                  <ListItem key={item.id}>
-                    <ListItemIcon sx={{ minWidth: "unset" }}>
-                      <ArrowForwardIosIcon />
-                    </ListItemIcon>
-                    <Link
-                      href="/"
-                      onClick={(e) => handleClick(e, item)}
-                      color="inherit"
-                      variant="body1"
-                      underline="hover"
-                      sx={(theme) => ({ fontSize: "1.5rem" })}
-                    >
-                      {item.name}
-                    </Link>
-                  </ListItem>
-                );
-              })}
-            </List>
-          </>
-        ) : (
-          <>
-            <List>
-              {types.map((item) => {
-                return (
-                  <ListItem
-                    key={item}
-                    sx={{
-                      flexDirection: "column",
-                      alignItems: "start",
-                      pl: 0,
-                    }}
-                  >
-                    <Typography variant="h4">{item}</Typography>
-                    <List>
-                      {names
-                        .filter((name) => name.type === item)
-                        .map((creature) => {
-                          return (
-                            <ListItem key={creature.id}>
-                              <ListItemIcon sx={{ minWidth: "unset" }}>
-                                <ArrowForwardIosIcon />
-                              </ListItemIcon>
-                              <Link
-                                href="/"
-                                onClick={(e) => handleClick(e, creature)}
-                                color="inherit"
-                                variant="body1"
-                                underline="hover"
-                                sx={(theme) => ({ fontSize: "1.5rem" })}
-                              >
-                                {creature.name}
-                              </Link>
-                            </ListItem>
-                          );
-                        })}
-                    </List>
-                  </ListItem>
-                );
-              })}
-            </List>
-          </>
-        )}
+        <Box sx={{ overflowY: "auto" }} className="scrollbar" id="style-7">
+          {sort === "name" ? (
+            <>
+              <List>
+                {names
+                  .sort((a, b) =>
+                    order
+                      ? a.name > b.name
+                        ? 1
+                        : -1
+                      : a.name > b.name
+                      ? -1
+                      : 1
+                  )
+                  .map((item) => {
+                    return (
+                      <ListItem key={item.id}>
+                        <ListItemIcon sx={{ minWidth: "unset" }}>
+                          <WhatshotIcon />
+                        </ListItemIcon>
+                        <Link
+                          href="/"
+                          onClick={(e) => handleClick(e, item)}
+                          color="inherit"
+                          variant="body1"
+                          underline="hover"
+                          sx={(theme) => ({ fontSize: "1.5rem" })}
+                        >
+                          {item.name}
+                        </Link>
+                      </ListItem>
+                    );
+                  })}
+              </List>
+            </>
+          ) : (
+            <>
+              <List>
+                {types
+                  .sort((a, b) => (order ? (a > b ? 1 : -1) : a > b ? -1 : 1))
+                  .map((item) => {
+                    return (
+                      <ListItem
+                        key={item}
+                        sx={{
+                          flexDirection: "column",
+                          alignItems: "start",
+                          pl: 0,
+                        }}
+                      >
+                        <Typography variant="h4">{item}</Typography>
+                        <List>
+                          {names
+                            .filter((name) => name.type === item)
+                            .sort((a, b) =>
+                              order
+                                ? a.name > b.name
+                                  ? 1
+                                  : -1
+                                : a.name > b.name
+                                ? -1
+                                : 1
+                            )
+                            .map((creature) => {
+                              return (
+                                <ListItem key={creature.id}>
+                                  <ListItemIcon sx={{ minWidth: "unset" }}>
+                                    <WhatshotIcon />
+                                  </ListItemIcon>
+                                  <Link
+                                    href="/"
+                                    onClick={(e) => handleClick(e, creature)}
+                                    color="inherit"
+                                    variant="body1"
+                                    underline="hover"
+                                    sx={(theme) => ({ fontSize: "1.5rem" })}
+                                  >
+                                    {creature.name}
+                                  </Link>
+                                </ListItem>
+                              );
+                            })}
+                        </List>
+                      </ListItem>
+                    );
+                  })}
+              </List>
+            </>
+          )}
+        </Box>
       </Drawer>
     </Box>
   );
